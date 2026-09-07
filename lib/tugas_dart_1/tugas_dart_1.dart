305d4de77a4b204314415d8a9356c3e1f5d6bddb
Dede nurhidayat <ranmaniax@gmail.com>
2026-09-07 09:28:45 +0700

    revisi tugas 1

--- lib/tugas_dart_1/tugas_dart_1.dart
+++ lib/tugas_dart_1/tugas_dart_1.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 /* 
   ini adalah tugas 1 dart yang saya buat, saya menulis beberapa variabel 
   */
@@ -10,12 +12,16 @@ void main() {
     'JavaScript: The Definitive Guide',
     'Minna no nihonggo',
   ];
+  Map<String, String> informasiTambahan = {
+    'alamat': "Koja, Jakarta Utara",
+    'Profesi': "Pelajar",
+  };
 
   /// function ini digunakan untuk print semua variabel
   void tampilkanBiodata() {
     //disini kita print nama
     print(
-      'Nama saya $nama, umur saya $umur, tinggi badan saya $tinggiBadan, status saya aktif = $statusAktif, daftar buku yang saya sukai : $daftarBukuFavorit',
+      'Nama saya $nama, umur saya $umur, tinggi badan saya $tinggiBadan, status saya aktif = $statusAktif, daftar buku yang saya sukai : $daftarBukuFavorit, alamat dan profesi saya : $informasiTambahan',
     );
   }
 
