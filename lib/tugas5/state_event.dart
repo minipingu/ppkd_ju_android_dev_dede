c547f274936fd943662a48fcfaf0ad5a8f49c6e5
minipingu <ranmaniax@gmail.com>
2026-09-09 22:10:56 +0700

    inkwell done

--- lib/tugas5/state_event.dart
+++ lib/tugas5/state_event.dart
@@ -18,7 +18,7 @@ class _StateEventState extends State<StateEvent> {
         : setState(() => elevatedButtonStatus = ButtonStatus.closed);
   }
 
-  //onPressed TextButton
+  //onPressed IconButton
   ButtonStatus iconButtonStatus = ButtonStatus.closed;
   void iconButtonEvent() {
     iconButtonStatus == ButtonStatus.closed
@@ -50,168 +50,203 @@ class _StateEventState extends State<StateEvent> {
         ),
         backgroundColor: Colors.blue,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Center(
-          child: Column(
-            spacing: 20,
-            children: [
-              // 1. Elevated Button
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    mainAxisAlignment: .center,
-                    children: [
-                      Text(
-                        'ElevatedButton',
-                        textAlign: .start,
-                        style: TextStyle(
-                          fontStyle: .italic,
-                          color: Colors.blueGrey,
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          elevatedButtonEvent();
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                          textStyle: TextStyle(fontSize: 20),
-                        ),
-                        child: Text(
-                          elevatedButtonStatus == ButtonStatus.closed
-                              ? 'Buka'
-                              : 'Tutup',
-                        ),
-                      ),
-                      if (elevatedButtonStatus == ButtonStatus.open)
-                        Column(
-                          children: [
-                            Text(
-                              "Halo, Saya ini WaPres-mu! 🗿",
-                              style: TextStyle(fontSize: 24),
-                            ),
-                            Image.asset('assets/images/gibs.png'),
-                          ],
-                        ),
-                    ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: Column(
+              spacing: 20,
+              children: [
+                // 1. Elevated Button
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(20),
                   ),
-                ),
-              ),
-              // 2. IconButton
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    mainAxisAlignment: .center,
-                    children: [
-                      Text(
-                        'IconButton',
-                        textAlign: .start,
-                        style: TextStyle(
-                          fontStyle: .italic,
-                          color: Colors.blueGrey,
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          iconButtonEvent();
-                        },
-                        icon: Icon(
-                          Icons.attach_money,
-                          color: iconButtonStatus == ButtonStatus.closed
-                              ? Colors.blue
-                              : Colors.red,
-                        ),
-                      ),
-                      if (iconButtonStatus == ButtonStatus.open)
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: .center,
+                      children: [
                         Text(
-                          'Kapan dollar turun wok? 😡',
-                          style: TextStyle(fontSize: 24),
+                          'ElevatedButton',
+                          textAlign: .start,
+                          style: TextStyle(
+                            fontStyle: .italic,
+                            color: Colors.blueGrey,
+                          ),
                         ),
-                    ],
-                  ),
-                ),
-              ),
-
-              // 3. TextButton
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-                    mainAxisAlignment: .center,
-                    children: [
-                      Text(
-                        'TextButton',
-                        textAlign: .start,
-                        style: TextStyle(
-                          fontStyle: .italic,
-                          color: Colors.blueGrey,
+                        ElevatedButton(
+                          onPressed: () {
+                            elevatedButtonEvent();
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.blue,
+                            foregroundColor: Colors.white,
+                            textStyle: TextStyle(fontSize: 20),
+                          ),
+                          child: Text(
+                            elevatedButtonStatus == ButtonStatus.closed
+                                ? 'Buka'
+                                : 'Tutup',
+                          ),
                         ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          textButtonEvent();
-                        },
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.blue,
-                          textStyle: TextStyle(fontSize: 20),
-                        ),
-                        child: Text(
-                          textButtonStatus == ButtonStatus.closed
-                              ? 'Buka'
-                              : 'Tutup',
-                        ),
-                      ),
-                      if (textButtonStatus == ButtonStatus.open)
-                        Column(
-                          children: [
-                            Text(
-                              "Hari ini saya mau pidato, tapi semoga saya tidak blunder.",
-                              style: TextStyle(fontSize: 18),
-                              textAlign: .justify,
-                            ),
-                            SizedBox(height: 20),
-                            Align(
-                              alignment: .centerStart,
-                              child: Text(
-                                "Ada pantun juga nih...",
-                                style: TextStyle(fontSize: 18),
+                        if (elevatedButtonStatus == ButtonStatus.open)
+                          Column(
+                            children: [
+                              Text(
+                                "Halo, Saya ini WaPres-mu! 🗿",
+                                style: TextStyle(fontSize: 24),
                               ),
-                            ),
-                            SizedBox(height: 20),
-                            Text(
-                              "Jalan-jalan ke pasar beli tomat, eh ngeliat foto terbang di langit.",
-                              style: TextStyle(fontSize: 18),
-                              textAlign: .justify,
-                            ),
-                            SizedBox(height: 20),
-                            Text(
-                              "Doakan semoga saya selamat, saya pamit 🗿",
-                              style: TextStyle(fontSize: 18),
-                              textAlign: .justify,
-                            ),
-                          ],
-                        ),
-                    ],
+                              Image.asset('assets/images/gibs.png'),
+                            ],
+                          ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+                // 2. IconButton
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: .center,
+                      children: [
+                        Text(
+                          'IconButton',
+                          textAlign: .start,
+                          style: TextStyle(
+                            fontStyle: .italic,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            iconButtonEvent();
+                          },
+                          icon: Icon(
+                            Icons.attach_money,
+                            color: iconButtonStatus == ButtonStatus.closed
+                                ? Colors.blue
+                                : Colors.red,
+                          ),
+                        ),
+                        if (iconButtonStatus == ButtonStatus.open)
+                          Text(
+                            'Kapan dollar turun wok? 😡',
+                            style: TextStyle(fontSize: 24),
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+                // 3. TextButton
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: .center,
+                      children: [
+                        Text(
+                          'TextButton',
+                          textAlign: .start,
+                          style: TextStyle(
+                            fontStyle: .italic,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            textButtonEvent();
+                          },
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.blue,
+                            textStyle: TextStyle(fontSize: 20),
+                            // backgroundColor: Colors.black
+                          ),
+                          child: Text(
+                            textButtonStatus == ButtonStatus.closed
+                                ? 'Buka'
+                                : 'Tutup',
+                          ),
+                        ),
+                        if (textButtonStatus == ButtonStatus.open)
+                          Column(
+                            children: [
+                              Text(
+                                "Hari ini saya mau pidato, tapi semoga saya tidak blunder.",
+                                style: TextStyle(fontSize: 18),
+                                textAlign: .justify,
+                              ),
+                              SizedBox(height: 20),
+                              Align(
+                                alignment: .centerStart,
+                                child: Text(
+                                  "Ada pantun juga nih...",
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ),
+                              SizedBox(height: 20),
+                              Text(
+                                "Jalan-jalan ke pasar beli tomat, eh ngeliat foto terbang di langit.",
+                                style: TextStyle(fontSize: 18),
+                                textAlign: .justify,
+                              ),
+                              SizedBox(height: 20),
+                              Text(
+                                "Doakan semoga saya selamat, saya pamit 🗿",
+                                style: TextStyle(fontSize: 18),
+                                textAlign: .justify,
+                              ),
+                            ],
+                          ),
+                      ],
+                    ),
+                  ),
+                ),
+                // 4. Inkwell
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisAlignment: .center,
+                      children: [
+                        Text(
+                          'InkWell',
+                          textAlign: .start,
+                          style: TextStyle(
+                            fontStyle: .italic,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () => print('Sentuhan terdeteksi'),
+                          child: Container(
+                            height: 200,
+                            width: 200,
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blue),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
