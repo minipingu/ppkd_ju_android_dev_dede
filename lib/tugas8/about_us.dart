cb56900846f66941ce2a8aadc642b1529c303a4d
minipingu <ranmaniax@gmail.com>
2026-09-17 00:12:43 +0700

    baru tugas 8, gara-gara research tentang package routing wkwkwk

--- lib/tugas8/about_us.dart
+++ lib/tugas8/about_us.dart
@@ -13,7 +13,44 @@ class _AboutUsState extends State<AboutUs> {
   Widget build(BuildContext context) {
     return AppScaffold(
       title: 'Tentang Kami',
-      body: Center(child: Text('asdasd')),
+      body: Padding(
+        padding: const EdgeInsets.all(60.0),
+        child: SizedBox(
+          width: .infinity,
+          height: .infinity,
+          child: Column(
+            mainAxisAlignment: .center,
+            crossAxisAlignment: .center,
+            spacing: 48,
+            children: [
+              Text(
+                'Tugas 8',
+                style: TextStyle(fontSize: 30, fontWeight: .bold),
+              ),
+              Text(
+                'Aplikasi ini dibuat dengan menggunakan package go router, karena saya tidak suka route berbasis index, biasanya pakai folder based yang lebih gampang, nanti lah di explore lagi, kemarin versi terbaru yg routefly masih ngebug. Sekian dan terima gaji. ps : Routing ini penting banget yah, sama pentingnya seperti data',
+                textAlign: .justify,
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                'by minipingu, cari aja di github',
+                textAlign: .center,
+                style: TextStyle(fontSize: 16, fontWeight: .bold),
+              ),
+              Text(
+                'versi? 0.000000001',
+                textAlign: .center,
+                style: TextStyle(fontSize: 16, fontWeight: .bold),
+              ),
+              Text(
+                'sekiannnnn......',
+                textAlign: .center,
+                style: TextStyle(fontSize: 16, fontStyle: .italic),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
